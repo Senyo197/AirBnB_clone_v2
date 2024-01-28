f05d2b1a8e1a034a716d35a133e151f21b640667
Senyo197 <Senyo197@gmail.com>
2024-01-28 21:48:41 +0000

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Falsk application that Returns Hello HBNB!
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Returns Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
