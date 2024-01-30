7fe69de2ef81fc271ff385fa0ce45547c0c7535b
Senyo197 <Senyo197@gmail.com>
2024-01-30 07:18:49 +0000

    number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -7,35 +7,36 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """Falsk application that Returns Hello HBNB!
     """
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Falsk application that Returns HBNB
     """
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """Falsk application that Returns text
     """
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route("/python/<text>", strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text="is cool"):
     """Falsk application that Returns text
     """
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """Falsk application that Returns number
     """
@@ -43,4 +44,4 @@ def number(n):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0' port=5000)
+    app.run(host='0.0.0.0', port=5000)
